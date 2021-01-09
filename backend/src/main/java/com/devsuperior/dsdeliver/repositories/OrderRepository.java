47c2e077425b4d16cdab70b946f99ad9e9fc671c
Dg medeiros <dgmedeiros42@outlook.com>
2021-01-09 00:38:11 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
@@ -10,9 +10,9 @@ import com.devsuperior.dsdeliver.entities.Order;
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
 	@Query("SELECT DISTINCT obj FROM Order obj JOIN FETCH obj.products "
-		    + " WHERE obj.status = 0 ORDER BY obj.moment ASC")
+		    + " WHERE obj.status = 0 Order BY obj.moment ASC")
 	
-          List<Order> findAllOrderWithProducts();
+          List<Order> findOrderWithProducts();
 }
 
  
